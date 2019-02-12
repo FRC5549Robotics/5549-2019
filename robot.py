713ec968870b984943663488f5c51a0158af20ac
Parsia B <parsiabahrami80@gmail.com>
2019-02-12 16:37:06 -0500

    Rev 0.5
    Addition of drive motors, sensors, and sub-systems for the 2019 Deep Space Competition.

--- robot.py
+++ robot.py
@@ -1,3 +1,7 @@
+'''
+Destination: Deep Space 2019 - GEMINI from Gryphon Robotics
+'''
+
 import wpilib
 import logging
 from math import *
@@ -5,78 +9,116 @@ from wpilib.drive import DifferentialDrive
 from wpilib import SmartDashboard
 from networktables import NetworkTables
 from ctre import *
+from navx import AHRS
+
 
 class MyRobot(wpilib.TimedRobot):
+    ''' values for navx'''
+    kP = 0.04
+    kI = 0.00
+    kD = 0.00
+    kF = 0.00
+
+    kToleranceDegrees = 2.0
+
     def robotInit(self):
-        self.frontRightMotor = WPI_TalonSRX(1)
+        ''' Initialization of robot objects. '''
+
+        ''' NavX '''
+        self.ahrs = AHRS.create_spi()
+
+        turnController = wpilib.PIDController(self.kP, self.kI, self.kD, self.kF, self.ahrs, output=self)
+        turnController.setInputRange(-180.0, 180.0)
+        turnController.setOutputRange(-0.6, 0.6)
+        turnController.setAbsoluteTolerance(self.kToleranceDegrees)
+        turnController.setContinuous(True)
+        self.turnController = turnController
+        self.rotateToAngleRate = 0
+
+        ''' Talon SRX Initialization '''
+        # drive train motors
+        self.frontRightMotor = WPI_TalonSRX(3)
         self.rearRightMotor = WPI_TalonSRX(2)
-        self.frontLeftMotor = WPI_TalonSRX(3)
-        self.rearLeftMotor = WPI_TalonSRX(4)
+        self.frontLeftMotor = WPI_TalonSRX(0)
+        self.rearLeftMotor = WPI_TalonSRX(1)
 
-        self.right_encoder = self.rearRightMotor
-        self.left_encoder = self.frontLeftMotor
-        self.P = 0.1
-        self.setpoint = 10000
+        # drive train encoders
+        self.rightEncoder = self.frontRightMotor
+        self.leftEncoder = self.frontLeftMotor
 
-        # defining motor groups
+        # drive train motor groups
         self.left = wpilib.SpeedControllerGroup(self.frontLeftMotor, self.rearLeftMotor)
         self.right = wpilib.SpeedControllerGroup(self.frontRightMotor, self.rearRightMotor)
 
-        # setting up drive group for drive motors
+        # drive train drive group
         self.drive = DifferentialDrive(self.left, self.right)
         self.drive.setExpiration(0.1)
 
-        # object that handles basic intake operations
-        self.omnom_left_motor = wpilib.Spark(7)
-        self.omnom_right_motor = wpilib.Spark(8)
+        ''' Victor SPX Initialization '''
+        # lift motors
+        self.liftOne = WPI_VictorSPX(0)
+        self.liftTwo = WPI_VictorSPX(1)
+        self.lift = wpilib.SpeedControllerGroup(self.liftOne, self.liftTwo)
 
-        # defining omnom motor groups
-        self.omnom_left = wpilib.SpeedControllerGroup(self.omnom_left_motor)
-        self.omnom_right = wpilib.SpeedControllerGroup(self.omnom_right_motor)
+        # lift arm motors
+        self.liftArmOne = WPI_VictorSPX(2)
+        self.liftArmTwo = WPI_VictorSPX(3)
+        self.liftArm = wpilib.SpeedControllerGroup(self.liftArmOne, self.liftArmTwo)
 
-        # setting up omnom group for omnom motors
-        self.omnom = DifferentialDrive(self.omnom_left, self.omnom_right)
-        self.omnom.setExpiration(0.1)
+        # cargo intake motor
+        self.cargo = WPI_VictorSPX(4)
 
-        # Lift motors
-        self.motorTopRight = wpilib.Victor(0)
-        self.motorTopLeft = wpilib.Victor(1)
-        self.motorBottomRight = wpilib.Victor(2)
-        self.motorBottomLeft = wpilib.Victor(3)
+        #lift encoders
+        self.liftEncoder = self.liftOne
 
-        # lift motor groups
-        self.lift_group = wpilib.SpeedControllerGroup(self.motorTopRight, self.motorTopLeft, self.motorBottomLeft, self.motorBottomRight)
+        #lift arm encoders
+        self.liftArmEncoder = self.liftArmOne
 
-        # joystick 0, 1, 2 on the driver station
+        ''' Controller Initialization '''
+        # joystick - 0, 1 | controller - 2
         self.leftStick = wpilib.Joystick(0)
         self.rightStick = wpilib.Joystick(1)
-        self.stick = wpilib.Joystick(2)
+        self.xbox = wpilib.Joystick(2)
 
+
+        ''' Pneumatic Initialization '''
+        self.Compressor = wpilib.Compressor(0)
+        self.Compressor.setClosedLoopControl(True)
+        self.enable = self.Compressor.getPressureSwitchValue()
+        self.DoubleSolenoidOne = wpilib.DoubleSolenoid(0, 1)
+        self.DoubleSolenoidTwo = wpilib.DoubleSolenoid(2, 3)
+        self.DoubleSolenoidThree = wpilib.DoubleSolenoid(4, 5)
+        self.SolenoidFan = wpilib.Solenoid(7)
+        self.Compressor.start()
+
+        # connection for logging & Smart Dashboard
         logging.basicConfig(level=logging.DEBUG)
         self.sd = NetworkTables.getTable('SmartDashboard')
         NetworkTables.initialize(server='10.55.49.2')
 
         self.timer = wpilib.Timer()
 
-        self.pdp = wpilib.PowerDistributionPanel()
-        self.robocontroller = wpilib.RobotController()
+        # Smart Dashboard classes
+        self.PDP = wpilib.PowerDistributionPanel()
+        self.roboController = wpilib.RobotController()
         self.DS = wpilib.DriverStation.getInstance()
-        self.timer = wpilib.Timer()
 
         # initialization of the HTTP camera
         wpilib.CameraServer.launch()
 
     def autonomousInit(self):
-        self.frontLeftMotor.setQuadraturePosition(0, 0)
-        self.rearRightMotor.setQuadraturePosition(0, 0)
+        ''' Executed each time the robot enters autonomous. '''
+
+        # encoder reset
 
     def autonomousPeriodic(self):
-        def pid():
-            self.rightPos = fabs(self.rearRightMotor.getQuadraturePosition())
-            self.leftPos = fabs(self.frontLeftMotor.getQuadraturePosition())
-            error = self.setpoint - ((self.leftPos + self.rightPos) / 2)
-            self.rcw = self.P * error
-            self.drive.arcadeDrive(0.3, self.rcw)
+        ''' Called periodically during autonomous. '''
+
+        def breakIn():
+            if self.timer.get() <= 600:
+                self.drive.tankDrive(1, 1)
+            else:
+                self.drive.tankDrive(0, 0)
 
         def encoder_test():
             self.rightPos = fabs(self.rearRightMotor.getQuadraturePosition())
@@ -87,30 +129,73 @@ class MyRobot(wpilib.TimedRobot):
             else:
                 self.drive.tankDrive(0, 0)
 
-        def auto_checkup():
-            pass
+        '''navx stuff'''
+        def navxTest():
+            if abs(self.ahrs.getAngle()) < 90.0:
+                self.drive.tankDrive(0.6, -0.6)
+            else:
+                self.drive.tankDrive(0, 0)
 
         if self.DS.getGameSpecificMessage() == "RRR":
-            encoder_test()
-        elif self.DS.getGameSpecificMessage() == "ACT":
-            auto_checkup()
+            breakIn()
 
     def teleopInit(self):
+        ''' Executed at the start of teleop mode. '''
         self.drive.setSafetyEnabled(True)
 
-        self.rearRightMotor.setQuadraturePosition(0, 0)
-        self.frontLeftMotor.setQuadraturePosition(0, 0)
+        # encoder reset
+
+        ''' NavX Reset '''
+        self.ahrs.reset()
 
     def teleopPeriodic(self):
-        # smart dashboard diagnostics
+        ''' Periodically executes methods during the teleop mode. '''
+
+        ''' NavX Control '''
+        self.sd.putNumber("Gyro angle: ", self.ahrs.getAngle())
+        rotateToAngle = False
+        if self.xbox.getRawButton(7):
+            self.ahrs.reset()
+        elif self.xbox.getRawButton(4):
+            self.turnController.setSetpoint(0.0)
+            rotateToAngle = True
+        elif self.xbox.getRawButton(2):
+            self.turnController.setSetpoint(90.0)
+            rotateToAngle = True
+        elif self.xbox.getRawButton(3):
+            self.turnController.setSetpoint(180.0)
+            rotateToAngle = True
+        elif self.xbox.getRawButton(1):
+            self.turnController.setSetpoint(-90.0)
+            rotateToAngle = True
+
+        if rotateToAngle:
+            self.turnController.enable()
+            currentRotationRate = self.rotateToAngleRate
+        else:
+            self.turnController.disable()
+            currentRotationRate = self.xbox.getX()
+
+        ''' Smart Dashboard '''
+        # Smart Dashboard diagnostics
         self.sd.putString("", "Diagnostics")
-        self.sd.putNumber("Temperature: ", self.pdp.getTemperature())
-        self.sd.putNumber("Battery Voltage: ", self.robocontroller.getBatteryVoltage())
-        self.sd.putBoolean(" Browned Out?", self.robocontroller.isBrownedOut())
+        self.sd.putNumber("Temperature: ", self.PDP.getTemperature())
+        self.sd.putNumber("Battery Voltage: ", self.roboController.getBatteryVoltage())
+        self.sd.putBoolean(" Browned Out?", self.roboController.isBrownedOut)
         self.sd.putBoolean(" Autonomous?", self.DS.isAutonomous())
         self.sd.putBoolean(" FMS Connection", self.DS.isFMSAttached())
-        self.sd.putNumber("Average Encoder Speed: ", self.rearRightMotor.getQuadratureVelocity())
+        self.sd.putNumber("Right Encoder Speed: ", abs(self.frontRightMotor.getQuadratureVelocity()))
+        self.sd.putNumber("Left Encoder Speed: ", abs(self.frontLeftMotor.getQuadratureVelocity()))
+        string = self.sd.getString("Number", "null")
+        self.sd.putString("String: ", string)
 
+        # Smart Dashboard encoder
+        self.RL = abs(self.rearLeftMotor.getQuadratureVelocity())
+        self.RR = abs(self.rearRightMotor.getQuadratureVelocity())
+        self.encoderAverage = ((self.RL + self.RR) / 2)
+        self.sd.putNumber("Average Encoder Speed: ", self.encoderAverage)
+
+        # Smart Dashboard match info
         self.sd.putString(" ", "Match Info")
         self.sd.putString("Event Name: ", self.DS.getEventName())
         self.sd.putNumber("Match Time: ", self.timer.getMatchTime())
@@ -132,27 +217,50 @@ class MyRobot(wpilib.TimedRobot):
         else:
             self.sd.putString("Alliance: ", "Invalid")
 
-        # lift controller mapping
-        if self.stick.getRawAxis(3):
-            self.lift_group.set(self.stick.getRawAxis(3) / 1.5)
-        elif self.stick.getRawAxis(2):
-            self.lift_group.set(self.stick.getRawAxis(2) / 40)
-        elif self.stick.getRawButton(5):
-            self.lift_group.set(self.stick.getRawButton(5) / 20)
+        ''' Pneumatics Control '''
+        # gear shifting
+        if self.rightStick.getRawButton(1):  # shift right
+            self.DoubleSolenoidOne.set(wpilib.DoubleSolenoid.Value.kForward)
+        elif self.leftStick.getRawButton(1):  # shift left
+            self.DoubleSolenoidOne.set(wpilib.DoubleSolenoid.Value.kReverse)
+
+        # hatch panel control
+        if self.rightStick.getRawButton(12):  # shift right
+            self.DoubleSolenoidTwo.set(wpilib.DoubleSolenoid.Value.kForward)
+        elif self.rightStick.getRawButton(11):  # shift left
+            self.DoubleSolenoidTwo.set(wpilib.DoubleSolenoid.Value.kReverse)
+
+        # hatch panel ejection
+        if self.leftStick.getRawButton(12):  # shift right
+            self.DoubleSolenoidThree.set(wpilib.DoubleSolenoid.Value.kForward)
+        elif self.leftStick.getRawButton(11):  # shift left
+            self.DoubleSolenoidThree.set(wpilib.DoubleSolenoid.Value.kReverse)
+
+        ''' Victor SPX (Lift, Lift Arm, Cargo) '''
+        # lift control
+        if self.xbox.getRawButton(1):
+            self.lift.set(0.5)
         else:
-            self.lift_group.set(0)
+            self.lift.set(0)
 
-        left_omnom_stick = self.stick.getRawAxis(5) / 1.25
-        right_omnom_stick = self.stick.getRawAxis(1) / 1.25
+        # lift arm control
+        if self.xbox.getRawButton(2):
+            self.liftArm.set(0.4)
+        else:
+            self.liftArm.set(0)
 
-        # drives intake system using tank steering
-        self.omnom.tankDrive(left_omnom_stick, right_omnom_stick)
+        # cargo intake control
+        if self.xbox.getRawButton(3):
+            self.cargo.set(0.5)
+        else:
+            self.cargo.set(0)
 
+        # controller mapping for tank steering
         rightAxis = self.rightStick.getRawAxis(1)
         leftAxis = self.leftStick.getRawAxis(1)
 
         # drives drive system using tank steering
-        self.drive.tankDrive(-leftAxis / 1.50, -rightAxis / 1.50)
+        self.drive.tankDrive(-leftAxis, -rightAxis)
 
 
 if __name__ == '__main__':
